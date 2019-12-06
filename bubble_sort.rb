6f8e50a3550ac6ade9c4f1a59a9badccfbc3222a
kitpao <kitpao91@gmail.com>
2019-12-06 14:57:14 -0600

    fix linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,12 @@
 def bubble_sort arr
   return arr if arr.size <= 1
-  len = arr.size-2
+
+  len = arr.size - 2
   while len > 1
     position = 0
     0.upto(len) do |i|
       if arr[i] > arr[i + 1]
-        arr[i], arr[i +1] = arr[i+ 1], arr[i]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         position = i
       end
     end
@@ -14,15 +15,16 @@ def bubble_sort arr
   arr
 end
 
-def bubble_sort_by arr
+def bubble_sort_by (arr)
   return arr if arr.size <= 1
-  len = arr.size-1
+
+  len = arr.size - 1
   while len >= 1
     position = 0
     len.times do |i|
-      comparison = yield arr[i], arr[i+1]
+      comparison = yield arr[i], arr[i + 1]
       if comparison.positive?
-        arr[i], arr[i +1] = arr[i+ 1], arr[i]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         position = i
       end
     end
@@ -32,6 +34,6 @@ def bubble_sort_by arr
 end
 
 arr = %w[hi hello hey]
-print bubble_sort_by(arr) {|left,right| left.length - right.length}
-unsorted = [10,4,6,8,5,2,4,6,8,9,22]
+print bubble_sort_by(arr) {|left, right| left.length - right.length}
+unsorted = [10, 4, 6, 8, 5, 2, 4, 6, 8, 9, 22]
 print bubble_sort unsorted
