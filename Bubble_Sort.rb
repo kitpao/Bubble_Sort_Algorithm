c4816b46f79aa7cf41df4417e2e405cf9d9e6249
kitpao <kitpao91@gmail.com>
2019-12-06 14:45:32 -0600

    add bubble_sort_by method

--- Bubble_Sort.rb
+++ Bubble_Sort.rb
@@ -1,7 +1,6 @@
 def bubble_sort arr
   return arr if arr.size <= 1
   len = arr.size-2
-  swapped = true
   while len > 1
     position = 0
     0.upto(len) do |i|
@@ -15,4 +14,24 @@ def bubble_sort arr
   arr
 end
 
-print bubble_sort [10,4,6,8,5,2,4,6,8,9,22]
+def bubble_sort_by arr
+  return arr if arr.size <= 1
+  len = arr.size-1
+  while len >= 1
+    position = 0
+    len.times do |i|
+      comparison = yield arr[i], arr[i+1]
+      if comparison.positive?
+        arr[i], arr[i +1] = arr[i+ 1], arr[i]
+        position = i
+      end
+    end
+    len = position
+  end
+  arr
+end
+
+arr = %w[hi hello hey]
+print bubble_sort_by(arr) {|left,right| left.length - right.length}
+unsorted = [10,4,6,8,5,2,4,6,8,9,22]
+print bubble_sort unsorted
