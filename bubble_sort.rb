b27a65889094ecda34efbee2dd8136ca0eb97689
kitpao <kitpao91@gmail.com>
2020-05-31 01:16:58 +0000

    Fix problem missing the last iteration in bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,11 +2,11 @@
 
 def bubble_sort(arr)
   return arr if arr.size <= 1
-
-  len = arr.size - 2
-  while len > 1
+  
+  len = arr.size - 1
+  while len >= 1
     position = 0
-    0.upto(len) do |i|
+    len.times do |i|
       if arr[i] > arr[i + 1]
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         position = i
@@ -39,3 +39,4 @@ arr = %w[hi hello hey]
 print bubble_sort_by(arr) { |left, right| left.length - right.length }
 unsorted = [10, 4, 6, 8, 5, 2, 4, 6, 8, 9, 22]
 print bubble_sort unsorted
+print bubble_sort [1, 0]
